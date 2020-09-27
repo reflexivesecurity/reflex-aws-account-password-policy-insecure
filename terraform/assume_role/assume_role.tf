6e84574058eb04954ff73466c20cee4c3b1f8b6d
rjulian <richard@rjulian.net>
2020-09-26 19:30:00 -0700

    Remove unnecessary rule suffix from name variables

--- terraform/assume_role/assume_role.tf
+++ terraform/assume_role/assume_role.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 module "assume_role" {
   source = "git::https://github.com/reflexivesecurity/reflex-engine.git//modules/sqs_lambda/modules/iam_assume_role?ref=v2.1.0"
 
-  function_name        = "AccountPasswordPolicyInsecureRule"
+  function_name        = "AccountPasswordPolicyInsecure"
   custom_lambda_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -19,6 +19,6 @@ module "assume_role" {
 }
 EOF
 
-  lambda_execution_role_arn = "arn:aws:iam::${var.parent_account}:role/ReflexAccountPasswordPolicyInsecureRuleLambdaExecution"
+  lambda_execution_role_arn = "arn:aws:iam::${var.parent_account}:role/ReflexAccountPasswordPolicyInsecureLambdaExecution"
 
 }
